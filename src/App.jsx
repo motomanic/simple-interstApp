3975a7c0d1bf6c324df4ee6c4a75e3d33a0ba40e
Rahul Raj <motomanic>
2024-12-17 15:00:35 +0530

    interst

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{width: '100%', minHeight: '100vh'}} className='d-flex justify-content-center align-items-center bg-dark'>
+      <div style={{width:'600px'}} className='bg-light rounded p-5'>
+      <h3>Simpe Interest App</h3>
+      <p>Calculate your simple Interest Easily</p>
+      <div className='bg-warning p-5 rounded'>
+      <h1>1500</h1>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
